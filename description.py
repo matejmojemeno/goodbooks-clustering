dc6c90c14c5a676411a7d8520f2a36aadd67c9b6
Matej Kulháň <kulhama8@cvut.cz>
2024-12-06 18:47:48 +0100

    Prepared the description column for transformers

--- description.py
+++ description.py
@@ -9,9 +9,10 @@ def process_description(text: str, nlp) -> str:
 
 
 def clean_description(description: pd.Series) -> pd.Series:
-    # language = description.apply(lambda x: langid.classify(x)[0])
-    # description = pd.Series(description[language == "en"])
+    language = description.apply(lambda x: langid.classify(x)[0])
+    description = pd.Series(description[language == "en"])
 
+    description = description.str.replace("'", "")
     description = description.str.replace("[^a-zA-Z]", " ", regex=True)
     description = description.str.replace("\s+", " ", regex=True).str.strip()
     description = description.str.lower()
@@ -31,9 +32,13 @@ def main():
     books["description_clean"] = None
     description: pd.Series = books.description.dropna()
 
+    print(description.shape)
+
     print(description.loc[0])
     description = clean_description(description)
     print(description.loc[0])
+
+    print(description.shape)
 
     books.loc[description.index, "description_clean"] = description
     books.to_csv("./goodbooks-10k/books_enriched.csv", index=False)
