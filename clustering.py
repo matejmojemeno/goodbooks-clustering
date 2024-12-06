c39dafac2a5d9da97ee8512688e57e27c8df707d
Matej Kulháň <kulhama8@cvut.cz>
2024-12-06 18:49:05 +0100

    Clustering first attempt

--- clustering.py
+++ clustering.py
@@ -4,8 +4,9 @@ import langid
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sentence_transformers import SentenceTransformer
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import MinMaxScaler, normalize
 
 
 def load_data() -> pd.DataFrame:
@@ -26,18 +27,18 @@ def load_data() -> pd.DataFrame:
             "ratings_5",
             "language_code",
             "publishDate",
+            "description",
+            "authors",
+            "best_book_id",
+            "work_id",
+            "goodreads_book_id",
+            "book_id",
+            "title",
+            "original_title",
         ],
         inplace=True,
     )
     print("Loaded dataset, shape: ", books.shape)
-    return books
-
-
-def filter_non_english(books: pd.DataFrame) -> pd.DataFrame:
-    books.dropna(subset=["description"], inplace=True)
-    books["language"] = books["description"].apply(lambda x: langid.classify(x)[0])
-    books = books[books["language"] == "en"].drop(columns=["language"])
-    print("Filtered non-english books, shape: ", books.shape)
     return books
 
 
@@ -46,26 +47,66 @@ def preprocess_genres(books: pd.DataFrame) -> pd.DataFrame:
     books["genres"] = books["genres"].apply(lambda x: x.split("'")[1::2])
 
     for group in genre_groups:
-        books[group] = (
-            books["genres"]
-            .apply(lambda x: 1 if len(set(x) & set(genre_groups[group])) > 0 else 0)
-            .astype("float")
+        books[group] = books["genres"].apply(
+            lambda x: len(set(x) & set(genre_groups[group])) > 0
         )
     books.drop(columns=["genres"], inplace=True)
-    books.loc[:, genre_groups.keys()] = books.loc[:, genre_groups.keys()].apply(
-        lambda x: x / x.sum(), axis=1
-    )
+    # books.loc[:, genre_groups.keys()] = books.loc[:, genre_groups.keys()].apply(
+    #     lambda x: x / x.sum(), axis=1
+    # )
 
     return books
 
 
+def create_embedding(books: pd.DataFrame) -> np.ndarray:
+    sentence_transformer = SentenceTransformer("all-MiniLM-L6-v2")
+    sentences = list(books["description_clean"])
+    embeddings = sentence_transformer.encode(sentences)
+    embeddings = normalize(embeddings)
+    return embeddings
+
+
+def dataset_for_clustering(books: pd.DataFrame) -> np.ndarray:
+    scaler = MinMaxScaler()
+    numerical_features = books[["average_rating", "pages", "ratings_count"]]
+    scaled_numerical_features = scaler.fit_transform(numerical_features)
+
+    genre_features = books[
+        [
+            "Young Adult",
+            "Graphic Media",
+            "Science and Fantasy",
+            "Modern Fiction",
+            "Literary Classics",
+            "Historical Fiction",
+            "Mystery and Thrillers",
+            "Nonfiction",
+        ]
+    ].values
+
+    embeddings = np.array(list(books["embeddings"]))
+
+    dataset = np.hstack([scaled_numerical_features, genre_features, embeddings])
+    return dataset
+
+
 def main():
     books = load_data()
-    books = filter_non_english(books)
+    books = books.dropna(subset=["description_clean"])
     books = preprocess_genres(books)
+    embeddings = create_embedding(books)
 
-    genre_groups = json.load(open("./data/genre_groups.json"))
-    print(books.loc[:, genre_groups.keys()].head())
+    books["embeddings"] = list(embeddings)
+    books.drop(columns=["description_clean"], inplace=True)
+    books.dropna(inplace=True)
+
+    dataset = dataset_for_clustering(books)
+    print(dataset.shape)
+
+    kmeans = KMeans(n_clusters=10, random_state=0).fit(dataset)
+    books["cluster"] = kmeans.labels_
+
+    print(books["cluster"].value_counts())
 
 
 if __name__ == "__main__":
